c9090b42a7db5b1ccc1a4e756bc7db4683a70eaf
SammoHungKINPOKO <h0534jokt@gmail.com>
2018-01-02 03:25:03 +0900

    first commit: HelloWorld状態

---
+++ src/main/java/com/shimofuri/damashiuchi/DamashiuchiApplication.java
@@ -0,0 +1,12 @@
+package com.shimofuri.damashiuchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamashiuchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamashiuchiApplication.class, args);
+	}
+}
